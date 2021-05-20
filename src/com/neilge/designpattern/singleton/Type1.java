afa4303b5089ce05e6091767024019962e13baf7
neilge <30841635+neilge@users.noreply.github.com>
2021-05-20 11:51:22 -0700

    Update Type1.java
    Add comments.

--- src/com/neilge/designpattern/singleton/Type1.java
+++ src/com/neilge/designpattern/singleton/Type1.java
@@ -9,13 +9,17 @@ public class Type1 {
   }
 }
 
+// 饿汉式（静态变量）
+
 class Singleton {
 
-  // 1. private constructor to prevent creating an instance from constructor
+  // 1. 构造器私有化， 不可以new
   private Singleton() {}
 
+  // 2. 内部创建对象实例
   private final static Singleton instance = new Singleton();
 
+  // 3. 提供一个公有的静态方法，返回实例对象
   public static Singleton getInstance() {
     return instance;
   }
